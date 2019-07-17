db0afadd59e207657362c93848b084a448c7f20e
yaqootturman <yaqoot.turman@gmail.com>
2019-07-17 22:01:12 +0300

    override div to avoiding write under each other

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const http = require("http");
+const http = require('http');
 const PORT = process.env.PORT || 4001;
-const LOCALHOST = process.env.LOCALHOST || "localhost";
-const router = require("./router");
+const LOCALHOST = process.env.LOCALHOST || 'localhost';
+const router = require('./router');
 
 http.createServer(router).listen(PORT, LOCALHOST, () => {
-	console.log(`Server running on http://${LOCALHOST}:${PORT}`);
+  console.log(`Server running on http://${LOCALHOST}:${PORT}`);
 });
