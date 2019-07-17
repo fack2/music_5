cf7ba2550a2c28cd7ba9fe4012daba8eee378585
95Rawan <youlostme17@hotmail.com>
2019-07-17 16:41:13 +0300

    dom response data to deal with

--- src/router.js
+++ src/router.js
@@ -1,27 +1,17 @@
-const { homeHandler, publicHandler, searchHandler } = require('./handler');
+const { homeHandler, publicHandler, searchHandler } = require("./handler");
 
 const router = (request, response) => {
-  if (request.url === '/') {
-    // response.writeHead(200, { 'Content-Type': 'text/html' });
-    // response.end('<h1>hello</h1>');
-    //isn`t? and above should be in handler , to load main hrml page
-    homeHandler(request, response);
-  } else if (request.url.includes('/search')) {
-    const obj = {
-      bannerImg: 'strArtistBanner',
-      strStyle: 'strStyle',
-      arrayOfSongs: 'arrayOfSongs',
-      strTrack: 'strTrack'
-    };
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(obj));
-    searchHandler(request, response, request.url);
-  } else if (request.url.includes('public')) {
-    publicHandler(request, response, request.url);
-  } else {
-    response.writeHead(404, { 'Content-Type': 'text/html' });
-    response.end('<h1>Sorry, page not found</h1>');
-  }
+	if (request.url === "/") {
+		response.writeHead(200, { "Content-Type": "text/html" });
+		homeHandler(request, response);
+	} else if (request.url.includes("/search")) {
+		searchHandler(request, response, request.url);
+	} else if (request.url.includes("public")) {
+		publicHandler(request, response, request.url);
+	} else {
+		response.writeHead(404, { "Content-Type": "text/html" });
+		response.end("<h1>Sorry, page not found</h1>");
+	}
 };
 
 module.exports = router;
--- src/test.js
+++ src/test.js
@@ -1,46 +1,46 @@
-const supertest = require('supertest');
-const test = require('tape');
-const router = require('./router');
+const supertest = require("supertest");
+const test = require("tape");
+const router = require("./router");
 
-test('Testing home endpoint', t => {
-  supertest(router)
-    .get('/')
-    .expect(200)
-    .end((error, response) => {
-      t.equal(response.statusCode, 200, 'Should be 200');
-      t.end();
-    });
+test("Testing home endpoint", t => {
+	supertest(router)
+		.get("/")
+		.expect(200)
+		.end((error, response) => {
+			t.equal(response.statusCode, 200, "Should be 200");
+			t.end();
+		});
 });
 
-test('Testing search endpoint', t => {
-  const obj = {
-    bannerImg: 'strArtistBanner',
-    strStyle: 'strStyle',
-    arrayOfSongs: 'arrayOfSongs',
-    strTrack: 'strTrack'
-  };
-  supertest(router)
-    .get('/search')
-    .expect(200)
-    .expect('Content-Type', '/json/')
-    .end((error, response) => {
-      t.deepEqual(response.body, obj, 'Should be an obj');
-      t.end();
-    });
+test("Testing search endpoint", t => {
+	const obj = {
+		bannerImg: "strArtistBanner",
+		strStyle: "strStyle",
+		arrayOfSongs: "arrayOfSongs",
+		strTrack: "strTrack"
+	};
+	supertest(router)
+		.get("/search")
+		.expect(200)
+		.expect("Content-Type", "/json/")
+		.end((error, response) => {
+			t.deepEqual(response.body, obj, "Should be an obj");
+			t.end();
+		});
 });
 
-test('Testing search invalid endpoints', t => {
-  supertest(router)
-    .get('/hi')
-    .expect(404)
-    .expect('Content-Type', '/html/')
-    .end((error, response) => {
-      t.equal(response.statusCode, 404, 'Should be 404');
-      t.equal(
-        response.text,
-        '<h1>Sorry, page not found</h1>',
-        'Should be page not found'
-      );
-      t.end();
-    });
+test("Testing search invalid endpoints", t => {
+	supertest(router)
+		.get("/hi")
+		.expect(404)
+		.expect("Content-Type", "/html/")
+		.end((error, response) => {
+			t.equal(response.statusCode, 404, "Should be 404");
+			t.equal(
+				response.text,
+				"<h1>Sorry, page not found</h1>",
+				"Should be page not found"
+			);
+			t.end();
+		});
 });
