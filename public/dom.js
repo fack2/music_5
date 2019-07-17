3d16e5687cac7bbe6134e05fd34c22c1c6eabf02
yaqootturman <yaqoot.turman@gmail.com>
2019-07-18 00:28:56 +0300

    add a videa as a frame nut it still need edit

--- public/dom.js
+++ public/dom.js
@@ -1,45 +1,52 @@
 const result = (url, cb) => {
-	fetch(url)
-		.then(response => {
-			return response.json();
-		})
-		.then(data => {
-			return cb(data);
-		})
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      return cb(data);
+    })
 
-		.catch(error => {
-			console.log(error);
-		});
+    .catch(error => {
+      console.log(error);
+    });
 };
 
-const searchButton = document.getElementById("button");
-searchButton.addEventListener("click", function(e) {
-	e.preventDefault();
-	const ul = document.getElementById("ul");
-	document.body.style.backgroundImage = "none";
-	const input = document.getElementById("input");
-	const inputValue = input.value;
-	if (inputValue.length === 0 || inputValue === /\S/) {
-		alert("Please enter an artist name");
-	} else {
-		result(`/search?${inputValue}`, data => {
-			const image = document.getElementById("bannerImg");
-			image.setAttribute("src", data.bannerImg);
-			// const strStyle = document.createElement("h3");
-			// strStyle.innerHTML = data.strStyle;
+const searchButton = document.getElementById('button');
+searchButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  const ul = document.getElementById('ul');
+  document.body.style.backgroundImage = 'none';
+  const input = document.getElementById('input');
+  const inputValue = input.value;
+  if (inputValue.length === 0 || inputValue === /\S/) {
+    alert('Please enter an artist name');
+  } else {
+    result(`/search?${inputValue}`, data => {
+      const image = document.getElementById('bannerImg');
+      image.setAttribute('src', data.bannerImg);
 
-			for (
-				let i = 0;
-				i < data.arrayOfSongs.length && i < data.strTrack.length;
-				i++
-			) {
-				const list = document.createElement("LI");
-				const songTitle = document.createElement("h1");
-				list.innerText = data.arrayOfSongs[i];
-				songTitle.innerText = data.strTrack[i];
-				ul.appendChild(list);
-				ul.appendChild(songTitle);
-			}
-		});
-	}
+      for (
+        let i = 0;
+        i < data.arrayOfSongs.length && i < data.strTrack.length;
+        i++
+      ) {
+        const list = document.createElement('LI');
+        const songTitle = document.createElement('h1');
+
+        // list.innerText = data.arrayOfSongs[i];
+        var vid = document.createElement('iframe');
+
+        const newUrl = data.arrayOfSongs[i].replace('watch', 'embed');
+
+        vid.src = newUrl;
+        songTitle.innerText = data.strTrack[i];
+
+        list.appendChild(vid);
+        ul.appendChild(list);
+        ul.appendChild(songTitle);
+      }
+    });
+    ul.innerHTML = '';
+  }
 });
